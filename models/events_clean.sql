560414f308934dc02655687b2fac241648b0a257
ciscomsk <kuznetzoff@gmail.com>
2025-08-25 16:35:01 +0300

    Mission_8_4

--- models/events_clean.sql
+++ models/events_clean.sql
@@ -1,11 +1,17 @@
+{% set date = var("date", none) %}
 SELECT DISTINCT
     user_id,
     timestamp,
     type_id,
     {{ updated_at() }}
 FROM {{ source("scooters_raw", "events") }}
+WHERE
 {% if is_incremental() %}
-    WHERE timestamp > (SELECT MAX(timestamp) FROM {{ this }})
+    {% if date %}
+        DATE(timestamp) = DATE '{{ date }}'
+    {% else %}
+        timestamp > (SELECT MAX(timestamp) FROM {{ this }})
+    {% endif %}
 {% else %}
-    WHERE timestamp < DATE '2023-08-01'
+    timestamp < TIMESTAMP '2023-08-01'
 {% endif %}
